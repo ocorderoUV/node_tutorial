469891b42d44ae76c65337bc8230047529f59cb9
ocorderoUV <ocordero@unitedvirtualities.com>
2020-02-05 18:06:31 -0700

    Autentication with Passport

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // app/index.js
 const calc = require('./calc')
 
+/*
 const numbersToAdd = [
   3,
   4,
@@ -10,3 +11,23 @@ const numbersToAdd = [
 
 const result = calc.sum(numbersToAdd)
 console.log(`The result is: ${result}`)
+
+*/
+
+// file:app/index.js
+const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+const app = express()
+app.use(session({
+  store: new RedisStore({
+    url: config.redisStore.url
+  }),
+  secret: config.redisStore.secret,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
